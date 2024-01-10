8564ecb294023678afe332ea8adf4cffae6350b4
spg-ali66 <ues22186@spengergasse.at>
2024-01-10 14:10:45 +0100

    weiter

--- frontend/usermanagement/src/main.js
+++ frontend/usermanagement/src/main.js
@@ -1,6 +1,8 @@
-import App from './App.vue'
+import App from '@/App.vue'
 import {createApp} from 'vue'
 import axios from 'axios'
+import process from 'process'; // Import the 'process' package
+
 axios.defaults.baseURL=process.env.NODE_ENV==='production'?'/api':'http://localhost:8080/api';
 
 import {registerPlugins} from '@/plugins'
