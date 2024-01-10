26215a6b5dd1dc6a20d09b23fec4b5e51989a711
spg-ali66 <ues22186@spengergasse.at>
2024-01-10 14:25:29 +0100

    button und input

---
+++ frontend/usermanagement/src/components/Button.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="btn-div">
+        <v-btn :type="type" class="submit-btn">{{ label }}</v-btn>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CustomButton',
+    props: {
+        type: {
+            type: String,
+            default: 'button',
+            validator: value => ['button', 'submit'].includes(value)
+        },
+        label: {
+            type: String,
+            default: 'Continue'
+        },
+    }
+};
+</script>
+
+<style scoped>
+    .submit-btn {
+        color: white;
+        background: black;
+        border-radius: 20px;
+        font-size: 10px;
+    }
+</style>
